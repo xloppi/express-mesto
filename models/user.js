86a7debaf461f347b14bfc57a0005af69235a1e2
xloppi <hlopkovalesha@yandex.ru>
2021-06-18 15:24:37 +0300

    add: user.js add fields: email, password

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+// const { celebrate, Joi } = require('celebrate');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -17,6 +19,22 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [isEmail, 'invalid email'],
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model('user', userSchema);
+
+/* Joi.string().required().unique().custom((value, helpers) => {
+  if (validator.isURL(value)) {
+    return value;
+  }
+  return helpers.error("invalid e"); */
