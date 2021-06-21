b280fc87880672635b06090333d0641d4d4dfedb
xloppi <hlopkovalesha@yandex.ru>
2021-06-21 12:57:33 +0300

    refoctor: another spoonful of validation

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,7 +6,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401).send({ message: 'Необходима авторизация 1' });
+    return res.status(401).send({ message: 'Необходима авторизация' });
   }
 
   const token = authorization.replace('Bearer ', '');
